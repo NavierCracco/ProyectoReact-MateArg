ecd867685f061a7231b5997e9142915dd96571ce
Navier Cracco <111366189+NavierCracco@users.noreply.github.com>
2023-04-18 19:57:27 -0300

    Actualizando url del logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className={styles.container}>
       <div className={styles.logo}>
         <NavLink to="/">
-          <img src="./img/logo-matearg-white.png" alt="logo" />
+          <img src="https://firebasestorage.googleapis.com/v0/b/ecommerce-d9504.appspot.com/o/imagenes%2Flogo-matearg-white.png?alt=media&token=5629d0f6-5608-48f6-ae9a-6d5973c5c945" alt="logo" />
         </NavLink>
         <NavLink className={styles.logoTitulo} to="/">
           <p>MateArg</p>
